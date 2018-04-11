446ee501bdcc18af8fcb56aae2098aaf3d4a97b8
anish <anish.moorthy@gmail.com>
2018-04-11 15:03:23 -0400

    Remove unnecessary boxing in Christian's Test

--- app/src/test/java/com/example/team38/ChristianTest.java
+++ app/src/test/java/com/example/team38/ChristianTest.java
@@ -7,20 +7,22 @@ import java.util.HashMap;
 import static org.junit.Assert.*;
 
 /**
- * Created by homosapien97 on 4/11/18.
+ * Test to make sure that shelters compare as equal when they should
  */
-
 public class ChristianTest {
+    /**
+     * @throws Exception When Something goes wrong
+     */
     @Test
     public void homeless_shelter_equals_is_correct() throws Exception {
         Object notAShelter = new Object();
         HashMap<String, Object> shelter_dictionary = new HashMap<>();
-        shelter_dictionary.put("id", new Long(0));
+        shelter_dictionary.put("id", 0L);
         shelter_dictionary.put("name", "a");
-        shelter_dictionary.put("capacity", new Long(100));
+        shelter_dictionary.put("capacity", 100L);
         shelter_dictionary.put("allowed", "men");
-        shelter_dictionary.put("latitude", new Double(20.0));
-        shelter_dictionary.put("longitude", new Double(30.0));
+        shelter_dictionary.put("latitude", 20.0);
+        shelter_dictionary.put("longitude", 30.0);
         shelter_dictionary.put("address", "SomeAddress");
         shelter_dictionary.put("services", "bed");
         shelter_dictionary.put("phone", "555-5555");
@@ -28,21 +30,21 @@ public class ChristianTest {
 
         //if(o instanceof HomelessShelter) {
             assertEquals(a, new HomelessShelter(shelter_dictionary)); //All equal
-            shelter_dictionary.put("id", new Long(1));
+            shelter_dictionary.put("id", 1L);
             assertNotEquals(a, new HomelessShelter(shelter_dictionary)); //id unequal
-            shelter_dictionary.put("id", new Long(0));
+            shelter_dictionary.put("id", 0L);
 
             shelter_dictionary.put("name", "b");
             assertNotEquals(a, new HomelessShelter(shelter_dictionary)); //name unequal
             shelter_dictionary.put("name", "a");
 
-            shelter_dictionary.put("latitude", new Double(80.0));
+            shelter_dictionary.put("latitude", 80.0);
             assertNotEquals(a, new HomelessShelter(shelter_dictionary)); //latitude unequal
-            shelter_dictionary.put("latitude", new Double(20.0));
+            shelter_dictionary.put("latitude", 20.0);
 
-            shelter_dictionary.put("longitude", new Double(90.0));
+            shelter_dictionary.put("longitude", 90.0);
             assertNotEquals(a, new HomelessShelter(shelter_dictionary)); //longitude unequal
-            shelter_dictionary.put("longitude", new Double(30.0));
+            shelter_dictionary.put("longitude", 30.0);
 
             shelter_dictionary.put("address", "OtherAddress");
             assertNotEquals(a, new HomelessShelter(shelter_dictionary)); //address unequal
