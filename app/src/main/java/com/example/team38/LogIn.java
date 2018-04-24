d63e3cb4de4f9e374d6272d407c0497c4bbafa1c
anish <anish.moorthy@gmail.com>
2018-04-24 00:36:26 -0400

    Implement lockout

--- app/src/main/java/com/example/team38/LogIn.java
+++ app/src/main/java/com/example/team38/LogIn.java
@@ -16,6 +16,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Properties;
+
 
 /**
  * Created by johnyi on 2/13/18.
@@ -81,6 +90,14 @@ public class LogIn extends AppCompatActivity {
      * @param view the thing to operate on
      */
     public void onLoginClicked(@SuppressWarnings("unused") View view) {
+
+        if (!canCurrentlyLogIn()) {
+            Toast.makeText(getApplicationContext(),
+                    "Locked out for " + String.valueOf(getRemainingLockoutTime()) + " seconds",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         Log.d("LoginScreen", "Login Button Pressed");
         final Intent intent = new Intent(this, ShelterListView.class);
         final EditText nameBox = findViewById(R.id.ID);
@@ -105,15 +122,19 @@ public class LogIn extends AppCompatActivity {
                             "").getValue(String.class);
                     if(pass.equals(pwd)) {
                         Log.d("LoginScreen", "Correct login!");
+                        refreshPropsOnSuccess();
 
                         User newCurrentUser = dataSnapshot.child(uid).getValue(User.class);
 
                         setCurrentUser(newCurrentUser);
                         startActivity(intent);
                     } else {
+                        consumeAttempt();
                         final Context context = getApplicationContext();
                         final int duration = Toast.LENGTH_SHORT;
-                        final Toast t = Toast.makeText(context, "incorrect password",
+                        final Toast t = Toast.makeText(context,
+                                "incorrect password: " + String.valueOf(getRemainingAttempts())
+                                + " remaining attempts before lockout",
                                 duration);
                         t.show();
                     }
@@ -132,8 +153,98 @@ public class LogIn extends AppCompatActivity {
         });
     }
 
+    private long getRemainingLockoutTime() {
+        Properties props = loadPropsFile();
+        long lockoutEnd = Long.parseLong(props.getProperty("LOCKOUT_END",
+                "0"));
+        long remaining = lockoutEnd - System.currentTimeMillis();
+        long secondsLeft = remaining / 1000;
+        if (secondsLeft < 0) {
+            secondsLeft = 0;
+        }
+        return secondsLeft;
+    }
+    private int getRemainingAttempts() {
+        Properties props = loadPropsFile();
+        int remaining_attempts = Integer.parseInt(props.getProperty("REMAINING_ATTEMPTS",
+                "0"));
+        return remaining_attempts;
+    }
+
     private static void setCurrentUser(User newCurrentUser) {
         User.currentUser = newCurrentUser;
     }
 
+    private Properties loadPropsFile() {
+        InputStream inputStream = null;
+
+        Properties props = new Properties();
+        try {
+            inputStream = openFileInput(Parameters.LOGIN_FILENAME);
+            props.load(inputStream);
+        } catch (Exception e) {
+            Log.d("login", "The internal login file was not found");
+            try {
+                inputStream = getResources().openRawResource(R.raw.loginfile);
+                props.load(inputStream);
+            } catch (IOException ee) {
+                e.printStackTrace();
+                Log.e("login", "Both login files were not found");
+                return null;
+            }
+        }
+        return props;
+    }
+
+    private boolean writePropsFile(Properties props) {
+
+        try {
+            FileOutputStream out = openFileOutput(Parameters.LOGIN_FILENAME, Context.MODE_PRIVATE);
+            props.store(out, "No comment");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("login", "Couldn't write to property file");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean canCurrentlyLogIn() {
+
+        Properties props = loadPropsFile();
+        if (props == null) {
+            return true;
+        }
+
+        int remainingTries = Integer.parseInt(props.getProperty("REMAINING_ATTEMPTS",
+                "3"));
+        long endOfLockout = Long.parseLong(props.getProperty("LOCKOUT_END", "0"));
+        if ((System.currentTimeMillis() >= endOfLockout)) {
+            if (remainingTries <= 0) {
+                remainingTries = 3;
+            }
+            props.setProperty("LOCKOUT_END", String.valueOf(0));
+            props.setProperty("REMAINING_ATTEMPTS", String.valueOf(remainingTries));
+            writePropsFile(props);
+        }
+        return (System.currentTimeMillis() > endOfLockout);
+    }
+    private void consumeAttempt() {
+        Properties props = loadPropsFile();
+
+        int remainingTries = Integer.parseInt(props.getProperty("REMAINING_ATTEMPTS",
+                "3"));
+        remainingTries -= 1;
+        if (remainingTries <= 0) {
+            long endOfLockout = System.currentTimeMillis() + Parameters.LOCKOUT_TIME;
+            props.setProperty("LOCKOUT_END", String.valueOf(endOfLockout));
+        }
+        props.setProperty("REMAINING_ATTEMPTS", String.valueOf(remainingTries));
+        writePropsFile(props);
+    }
+    private void refreshPropsOnSuccess() {
+        Properties props = loadPropsFile();
+        props.setProperty("REMAINING_ATTEMPTS", String.valueOf(3));
+        writePropsFile(props);
+    }
 }
--- app/src/main/java/com/example/team38/Parameters.java
+++ app/src/main/java/com/example/team38/Parameters.java
@@ -2,4 +2,6 @@ package com.example.team38;
 
 class Parameters {
     final static int WAIT_REFRESH_MILLIS = 500;
+    final static int LOCKOUT_TIME = 30 * 1000;
+    final static String LOGIN_FILENAME = "login.props";
 }
